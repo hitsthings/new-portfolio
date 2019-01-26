0565dda65c7948cfadea62f6cd573e563199627b
Caren Keyes <cejkeyes@gmail.com>
2019-01-25 19:13:48 -0800

    made project cards with front image that flip

--- src/Projects/project-card.js
+++ src/Projects/project-card.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 export default class ProjectCard extends React.Component{
     render(){
+        console.log(this.props.image)
         return(
-            <div className='project-card-wrapper'></div>
+            <div className='project-card-container'>
+                <div className='project-card'>
+                    <div className='project-card-front'>
+                        <img src={this.props.image} alt={this.props.alt}/>
+                    </div>
+                    <div className='project-card-back'>
+
+                    </div>
+                </div>
+            </div>
         )
     }
 }
--- src/Projects/projects.js
+++ src/Projects/projects.js
@@ -5,9 +5,19 @@ import ProjectCard from './project-card';
 export default function Projects(props){
     return(
         <div className='container projects-wrapper'>
-            <ProjectCard />
-            <ProjectCard />
-            <ProjectCard />
+            <ProjectCard 
+                image={require('../assets/something-new.png')} 
+                alt='screen shot of something new app'
+            />            
+            <ProjectCard
+                image={require('../assets/money-jars.png')} 
+                alt='screen shot of money jars app'
+            />
+
+            <ProjectCard 
+                image={require('../assets/family-reunion.png')} 
+                alt='screen shot of family reunion app'
+            />
         </div>
     )
 }
